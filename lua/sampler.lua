5840ef43fff4b2ff56459bc68b3b5132979104e3
pat <patrickcaulfield1177@gmail.com>
2021-11-04 16:05:17 +0000

    fixed sampler

--- lua/sampler.lua
+++ lua/sampler.lua
@@ -4,14 +4,14 @@ function sampler:new(name)
   self.__index = self
 
   local o = {}
-  o.read = 0
-  o.speed = 1
-  o.dur, o.chans, o.data = load_sample(name)
+  o.read = 0.0
+  o.speed = 1.0
+  o.length, o.chans, o.data = load_sample(name)
   o.value = {}
   o.loop = false
   o.cut = true
   o.active = false
-  for i = 1, o.chans do o.value[i] = 0 end
+  for i = 1, o.chans do o.value[i] = 0.0 end
 
   return setmetatable(o, self)
 end
@@ -20,17 +20,17 @@ function sampler:update()
   if self.active then
     local index = floor(self.read) * self.chans
 
-    for i in pairs(self.value) do self.value[i] = self.data[index + i] end
+    for i = 1, self.chans do self.value[i] = self.data[index + i] end
 
     self.read = self.read + self.speed
 
-    if abs(self.read) > self.dur then
+    if abs(self.read) > self.length then
       if self.loop then
-        self.read = self.read % self.dur
+        self.read = self.read % self.length
       else
-        self.read = 0
+        self.read = 0.0
         self.active = false
-        for i in pairs(self.value) do self.value[i] = 0 end
+        for i = 1, self.chans do self.value[i] = 0.0 end
       end
     end
   end
@@ -43,6 +43,6 @@ function sampler:trigger()
   if self.cut then self:reset() end
 end
 
-function sampler:reset() self.read = 0 end
+function sampler:reset() self.read = 0.0 end
 
 return sampler
